d29a72523ce5b8190ada83b78499bd1397b39f28
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2022-11-16 18:13:28 +0500

    Refactoring!

--- Records Routines/record_routines.sql
+++ Records Routines/record_routines.sql
@@ -1,4 +1,267 @@
 DELIMITER |
+CREATE FUNCTION function_check_if_publisher_exists_and_get_id(new_publisher VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SET @exists := EXISTS(SELECT publisher_id
+                          FROM table_publishers
+                          WHERE publisher = new_publisher);
+    IF @exists THEN
+        SELECT function_get_publisher_id(new_publisher) INTO id;
+        RETURN id;
+    ELSE
+        SELECT function_insert_publisher_and_get_id(new_publisher) INTO id;
+        RETURN id;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_publisher_id(new_publisher VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT publisher_id INTO id FROM table_publishers WHERE publisher = new_publisher;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_publisher_and_get_id(new_publisher VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_publishers (publisher) VALUE (new_publisher);
+    SELECT MAX(publisher_id) INTO id FROM table_publishers;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_check_if_media_format_exists_and_get_id(new_media_format VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SET @exists := EXISTS(SELECT media_format_id
+                          FROM table_media_formats
+                          WHERE media_format = new_media_format);
+    IF @exists THEN
+        SELECT function_get_media_format_id(new_media_format) INTO id;
+        RETURN id;
+    ELSE
+        SELECT function_insert_media_format_and_get_id(new_media_format) INTO id;
+        RETURN id;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_media_format_id(new_media_format VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT media_format_id INTO id FROM table_media_formats WHERE media_format = new_media_format;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_media_format_and_get_id(new_media_format VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_media_formats (media_format) VALUE (new_media_format);
+    SELECT MAX(media_format_id) INTO id FROM table_media_formats;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_check_if_genre_exists_and_get_id(new_genre VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SET @exists := EXISTS(SELECT genre_id
+                          FROM table_genres
+                          WHERE genre = new_genre);
+    IF @exists THEN
+        SELECT function_get_genre_id(new_genre) INTO id;
+        RETURN id;
+    ELSE
+        SELECT function_insert_genre_and_get_id(new_genre) INTO id;
+        RETURN id;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_genre_id(new_genre VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT genre_id INTO id FROM table_genres WHERE genre = new_genre;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_genre_and_get_id(new_genre VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_genres (genre) VALUE (new_genre);
+    SELECT MAX(genre_id) INTO id FROM table_genres;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_check_if_person_as_performer_exists(new_first_name VARCHAR(100), new_last_name VARCHAR(100))
+    RETURNS BOOL
+    DETERMINISTIC
+BEGIN
+    SET @exists := EXISTS(SELECT person_id
+                          FROM table_persons
+                          WHERE first_name = new_first_name
+                            AND last_name = new_last_name
+                            AND is_performer = TRUE);
+    IF @exists THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_person_as_performer_id(new_first_name VARCHAR(100), new_last_name VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT person_id
+    INTO id
+    FROM table_persons
+    WHERE first_name = new_first_name
+      AND last_name = new_last_name
+      AND is_performer = TRUE;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_person_as_performer_and_get_id(new_first_name VARCHAR(100), new_last_name VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_persons (first_name, last_name, is_performer) VALUES (new_first_name, new_last_name, TRUE);
+    SELECT MAX(person_id) INTO id FROM table_persons;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_check_if_performer_exists_and_get_id(new_first_name VARCHAR(100), new_last_name VARCHAR(100))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id_person INT;
+    DECLARE id_performer INT;
+    SET @exists := function_check_if_person_as_performer_exists(new_first_name, new_last_name);
+    IF @exists THEN
+        SELECT function_get_person_as_performer_id(new_first_name, new_last_name) INTO id_person;
+        SELECT function_get_performer_id(id_person) INTO id_performer;
+        RETURN id_performer;
+    ELSE
+        SELECT function_insert_person_as_performer_and_get_id(new_first_name, new_last_name) INTO id_person;
+        SELECT function_insert_performer_and_get_id(id_person) INTO id_performer;
+        RETURN id_performer;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_performer_id(new_person_id INT)
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT performer_id
+    INTO id
+    FROM table_performers
+    WHERE person_id = new_person_id;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_performer_and_get_id(new_person_id INT)
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_performers (person_id) VALUE (new_person_id);
+    SELECT MAX(performer_id) INTO id FROM table_performers;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_check_if_band_exists_and_get_id(new_band_name VARCHAR(255))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SET @exists := EXISTS(SELECT band_id
+                          FROM table_bands
+                          WHERE band_name = new_band_name);
+    IF @exists THEN
+        SELECT function_get_band_id(new_band_name) INTO id;
+        RETURN id;
+    ELSE
+        SELECT function_insert_band_and_get_id(new_band_name) INTO id;
+        RETURN id;
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_get_band_id(new_band_name VARCHAR(255))
+    RETURNS INT
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    SELECT band_id
+    INTO id
+    FROM table_bands
+    WHERE band_name = new_band_name;
+    RETURN id;
+END |
+
+
+DELIMITER |
+CREATE FUNCTION function_insert_band_and_get_id(new_band_name VARCHAR(255))
+    RETURNS BOOL
+    DETERMINISTIC
+BEGIN
+    DECLARE id INT;
+    INSERT INTO table_bands (band_name) VALUE (new_band_name);
+    SELECT MAX(band_id) INTO id FROM table_bands;
+    RETURN id;
+END |
+
+
+DELIMITER |
 CREATE PROCEDURE procedure_get_record_genres(IN id_record INT)
 BEGIN
     SELECT genre
@@ -95,4 +358,59 @@ BEGIN
     DECLARE records_count INT;
     SELECT MAX(record_id) INTO records_count FROM table_records;
     RETURN records_count;
+END |
+
+
+DELIMITER |
+CREATE PROCEDURE procedure_create_record_and_get_id(IN new_title VARCHAR(255),
+                                                    IN new_total_duration TIME, IN new_publisher VARCHAR(100),
+                                                    IN new_release_date CHAR(4),
+                                                    IN new_media_format VARCHAR(50), OUT new_record_id INT)
+BEGIN
+    INSERT INTO table_records (title, total_duration, publisher_id, release_year, media_format_id)
+    VALUES (new_title, new_total_duration,
+            function_check_if_publisher_exists_and_get_id(new_publisher), new_release_date,
+            function_check_if_media_format_exists_and_get_id(new_media_format));
+    SELECT MAX(record_id) INTO new_record_id FROM table_records;
+END |
+
+
+DELIMITER |
+CREATE PROCEDURE procedure_create_record_genre_item(IN new_record_id INT, IN new_genre VARCHAR(50))
+BEGIN
+    INSERT INTO table_record_genre_items (record_id, genre_id)
+    VALUES (new_record_id, function_check_if_genre_exists_and_get_id(new_genre));
+END |
+
+
+DELIMITER |
+CREATE PROCEDURE procedure_create_record_performer_item_with_performer(IN new_record_id INT,
+                                                                       new_first_name VARCHAR(100),
+                                                                       new_last_name VARCHAR(100))
+BEGIN
+    SET @id_performer := function_check_if_performer_exists_and_get_id(new_first_name, new_last_name);
+    SET @exists := EXISTS(SELECT record_performer_item_id
+                          FROM table_record_performer_items
+                          WHERE record_id = new_record_id
+                            AND performer_id = @id_performer);
+
+    IF !@exists THEN
+        INSERT INTO table_record_performer_items (record_id, performer_id)
+        VALUES (new_record_id, @id_performer);
+    END IF;
+END |
+
+
+DELIMITER |
+CREATE PROCEDURE procedure_create_record_performer_item_with_band(IN new_record_id INT, IN new_band_name VARCHAR(255))
+BEGIN
+    SET @id_band := function_check_if_band_exists_and_get_id(new_band_name);
+    SET @exists := EXISTS(SELECT record_performer_item_id
+                          FROM table_record_performer_items
+                          WHERE record_id = new_record_id
+                            AND band_id = @id_band);
+    IF !@exists THEN
+        INSERT INTO table_record_performer_items (record_id, band_id)
+        VALUES (new_record_id, @id_band);
+    END IF;
 END |
